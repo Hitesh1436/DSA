6a16d3b503ae0e674a36be6b05ab7d753d4127c1
Hitesh1436 <hsc253545@gmail.com>
2022-04-16 17:25:57 +0530

    Graph

--- Graph/Is Graph Connected/Main.java
+++ Graph/Is Graph Connected/Main.java
@@ -1,1 +1,57 @@
-a
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+   
+   public static void isConnected(ArrayList<Edge> []graph ,int src ,int dest ,boolean []vis){
+        vis[src]=true;
+        for(Edge e : graph[src]){
+            int nbr = e.nbr;
+            if(!vis[nbr]){
+                isConnected(graph,nbr,dest,vis);
+            }
+        }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      int src = 0;
+      int dest = graph.length-1;
+      boolean [] vis = new boolean[graph.length];
+      isConnected(graph,src,dest,vis);
+      if(vis[dest]== true){
+          System.out.print("true");
+      }else{
+          System.out.print("false");
+      }
+     
+   }
+}
